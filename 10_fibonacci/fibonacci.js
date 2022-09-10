ae6afd3ffa6f70fae3ee7cb6e16ae5720e0401ea
fabio-writes-code <fabiohc1@hotmail.com>
2022-09-10 15:20:00 -0600

    Add JS fibonacci finder

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
-
+const fibonacci = function(a) {
+    fib=[1,1]
+    if (a==1 || a==2){
+        return 1
+    }
+    if (a<=0){
+        return 'OOPS'
+    }
+    for (let i = 2; i <= a-1; i++) {
+        fib[i]=fib[i-1]+fib[i-2];
+    }
+    return fib[a-1]
 };
 
 // Do not edit below this line
