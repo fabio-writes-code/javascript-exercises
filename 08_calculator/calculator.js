1903bc646fcaf72b172ebfbe9b1c5f21bf143638
fabio-writes-code <fabiohc1@hotmail.com>
2022-09-10 14:23:05 -0600

    Adds calculator functions

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,37 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return a+b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a-b
 };
 
-const sum = function() {
-	
+const sum = function(A) {
+	total=0;
+  for (let index = 0; index < A.length; index++) {
+    total+=A[index];
+  }
+  return total;
 };
 
-const multiply = function() {
-
+const multiply = function(A) {
+  total=1
+  A.forEach(element => {
+    total*=element
+  });
+  return total
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	return Math.pow(a,b)
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  total=1
+	for (let i = a; i >0 ; i--){
+    total*=i;   
+  }
+  return total
 };
 
 // Do not edit below this line
