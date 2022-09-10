22ad9c7381574431bf4400877190c6ee9167ba83
fabio-writes-code <fabiohc1@hotmail.com>
2022-09-10 15:00:23 -0600

    Add palindrome functionality

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
-
+const palindromes = function (A) {
+    newString=''
+    A=A.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"");
+    A=A.replaceAll(' ','');
+    A=A.toLowerCase();
+    // console.log(A)
+    for (let i = 0; i < A.length; i++) {
+        if (A[i]!=A[A.length-1-i]){
+            return false
+        }
+    }
+    return true
 };
 
 // Do not edit below this line
