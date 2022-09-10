b81f791d998ac1858e8bbc0deda2a337405f34af
fabio-writes-code <fabiohc1@hotmail.com>
2022-09-10 17:18:06 -0600

    Add JS sort code

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(A) {
+    const oldest=A.sort((a,b)=>{
+        if (!b.yearOfDeath){
+            b.yearOfDeath=(new Date).getFullYear();
+        }
+        if (!a.yearOfDeath){
+            a.yearOfDeath=(new Date).getFullYear();
+        }
+        aAge=a.yearOfDeath-a.yearOfBirth;
+        bAge=b.yearOfDeath-b.yearOfBirth
+        return aAge<bAge?1:-1
+    })
+    return oldest[0]
 };
 
 // Do not edit below this line
